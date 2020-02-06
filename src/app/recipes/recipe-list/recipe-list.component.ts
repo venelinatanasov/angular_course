88057d3fb9d4640324ad84139aa8cc390e4b1d41
Venelin Atanasov <venelin.atanasov2@gmail.com>
2020-02-06 23:57:39 +0200

    recipe model defined and used, 3.11

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[]=[
+    new Recipe('Kyufte', 'Basi qkoto 4uek', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fic.pics.livejournal.com%2Falex_barbov%2F48771207%2F617405%2F617405_original.jpg&f=1&nofb=1'),
+    new Recipe('Kebapche', 'Mn qko 4uek', 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fitalianrentalblog.files.wordpress.com%2F2014%2F12%2F523996253.jpg%3Fw%3D470%26h%3D313&f=1&nofb=1'),
 
+
+
+
+
+
+  ];
   constructor() { }
 
   ngOnInit() {
